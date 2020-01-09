91e844e1717fc3e6147e1aa226c18b0234451ab8
sertmer <scottertmer@gmail.com>
2020-01-08 17:20:38 -0700

    Build initial LandingForm, create variables for fonts

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import LandingForm from '../LandingForm/LandingForm'
 import './App.scss';
 
 function App() {
   return (
     <body>
       <h1>hi</h1>
+      <LandingForm />
     </body>
   );
 }
---
+++ src/Components/LandingForm/LandingForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandingForm = () => {
+  return (
+    <main>
+      <h2>The Truth has Nine Lives</h2>
+      <button type='button' className='Login-button'>Join Us</button>
+    </main>
+  )
+}
+
+export default LandingForm
