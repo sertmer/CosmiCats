796d8c091b66f2feed1cbd5b696e57ce3ac35346
sertmer <scottertmer@gmail.com>
2020-01-08 20:13:15 -0700

    Build initial ResearchContainer

---
+++ src/Components/ResearchContainer/ResearchContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ResearchContainer.scss';
+import Research from '../Research/Research'
+
+export const ResearchContainer = () => {
+  return(
+    <section>
+      <article>
+        <h2>nasa id</h2>
+        <p>nas123</p>
+        <h2>date taken</h2>
+        <p>december</p>
+        <h2>photographer</h2>
+      </article>
+    </section>
+  )
+}
+
+export default ResearchContainer;
