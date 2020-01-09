2742c9f2f7faa94b9cf61bed582789721afec60d
sertmer <scottertmer@gmail.com>
2020-01-09 08:42:20 -0700

    Pass handleChange tests in login

--- src/Components/Login/Login.test.js
+++ src/Components/Login/Login.test.js
@@ -4,15 +4,35 @@ import { Login } from './Login';
 
 describe('Login', () => {
 
-  let wrapper
+  let wrapper, mockEvent
 
   beforeEach(() => {
+    mockEvent = { target: {name: 'name', value:'Davey Havok'} }
+
     wrapper = shallow(<Login />)
   })
 
-  describe('Login unit tests', () => {
+  describe('Unit Tests', () => {
     it('should match the snapshot', () => {
       expect(wrapper).toMatchSnapshot()
+    })
+
+    describe('handleChange', () => {
+      it('should call setState when handleChange is invoked', () => {
+        const expected = { name: 'Davey Havok' }
+        wrapper.instance().setState = jest.fn()
+        wrapper.instance().handleChange(mockEvent)
+
+        expect(wrapper.instance().setState).toHaveBeenCalledWith(expected)
+      })
+
+      it('should set state with name when handleChange is called', () => {
+        const expected = {name: 'Davey Havok', hasAgreed: false}
+        wrapper.setState({ name: '', hasAgreed: false });
+        wrapper.instance().handleChange(mockEvent);
+
+        expect(wrapper.state()).toEqual(expected)
+      });
     })
   })
 })
