2e84a6c5ea02b9bfa31abfd64f42eea580f68ec1
sertmer <scottertmer@gmail.com>
2020-01-09 12:58:19 -0700

    Add sign out to header

--- src/Containers/Header/Header.js
+++ src/Containers/Header/Header.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { setUser } from '../../actions';
 import './Header.scss';
 import alien from '../../images/alien.svg';
 import cat from '../../images/cat.svg';
 
-export const Header = ({user}) => {
+export const Header = ({user, setUser}) => {
   return (
     <header>
       <h1 className='cccr-header'>The Center for CosmiCat Research</h1>
-      { user && <h1>Welcome Dr.{user}</h1>}
+      { user && <div>
+          <h2>Welcome Dr.{user}</h2>
+          <Link to='/'>
+            <button onClick={() => setUser('')}>Sign Out</button>
+          </Link>
+        </div>
+      }
       <div className='icon-div'>
         <img className="icon" src={alien} alt='small green alien icon' />
         <p className='triple-equals'>===</p>
@@ -22,4 +30,8 @@ export const mapStateToProps = state => ({
   user: state.user
 })
 
-export default connect(mapStateToProps)(Header);
+export const mapDispatchToProps = dispatch => ({
+  setUser: user => dispatch(setUser(user))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/Containers/Login/Login.js
+++ src/Containers/Login/Login.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './Login.scss';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { setUser } from '../../actions'
+import { setUser } from '../../actions';
 
 export class Login extends Component {
   constructor(props) {
