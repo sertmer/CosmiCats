9f2191718d6eb5b7b5ad2bd84ee27bfb6faabf7a
sertmer <scottertmer@gmail.com>
2020-01-10 11:08:28 -0700

    Add and pass setNasaImages action creator test

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,6 +1,7 @@
 import * as actions from './index'
 
 describe('actions', () => {
+
   describe('SET_USER', () => {
     it('should have a type of SET_USER', () => {
       const user = 'Davey Havok'
@@ -11,6 +12,43 @@ describe('actions', () => {
       }
 
       expect(result).toEqual(expected)
+    })
+  })
+
+  describe('SET_NASA_IMAGES', () => {
+    it('should have a type of SET_NASA_IMAGES', () => {
+      const mockImages = [
+        {
+          href: 'someURL.com',
+          nasa_id: 'PIA123',
+          date_created: '2019-03-01'
+        }, 
+        {
+          href: 'somURL2.com',
+          nasa_id: 'DIZ123',
+          date_created: '2019-04-01'
+        }
+      ]
+
+      const mockExpectedAction = {
+        type: 'SET_NASA_IMAGES',
+        images: [
+          {
+            href: 'someURL.com',
+            nasa_id: 'PIA123',
+            date_created: '2019-03-01'
+          }, 
+          {
+            href: 'somURL2.com',
+            nasa_id: 'DIZ123',
+            date_created: '2019-04-01'
+          }
+        ]
+      }
+
+      const result = actions.setNasaImages(mockImages)
+      
+      expect(result).toEqual(mockExpectedAction)
     })
   })
 })
