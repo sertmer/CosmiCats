8c3eb49354ecb57f5b71709b91f4507c69ff9dc2
sertmer <scottertmer@gmail.com>
2020-01-12 16:20:32 -0700

    Add and pass mapDispatch test for Research

--- src/Containers/Research/Research.test.js
+++ src/Containers/Research/Research.test.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Research } from './Research';
+import { addToEvidence } from '../../actions'
+import { Research, mapDispatchToProps } from './Research';
 
 describe('Research', () => {
 
   let wrapper
+  let mockAddToEvidence
+  let mockCaseStudy
 
   beforeEach(() => {
-    wrapper = shallow(<Research />)
+    mockCaseStudy = {
+      img: 'url.com',
+      id: 'ab1',
+      dateCreated: '12-12-12'
+    }
+    wrapper = shallow(<Research caseStudy={mockCaseStudy}/>)
   })
 
   describe('Research unit tests', () => {
-    it.skip('should match the snapshot', () => {
+    it('should match the snapshot', () => {
       expect(wrapper).toMatchSnapshot()
+    })
+  })
+
+  describe('mapDispatchToProps', () => {
+    it('should call dispatch with the addToEvidence action', () => {
+      const mockEvidence = {
+        img: 'url.com',
+        id: 'ab1',
+        dateCreated: '12-12-12'
+      }
+  
+      const mockDispatch = jest.fn()
+      const mockAddToEvidenceAction = addToEvidence(mockEvidence)
+      const mappedProps = mapDispatchToProps(mockDispatch)
+  
+      mappedProps.addToEvidence(mockEvidence)
+  
+      expect(mockDispatch).toHaveBeenCalledWith(mockAddToEvidenceAction)
     })
   })
 })
