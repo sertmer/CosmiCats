54ab4c44942399d731fdf1439c4ffadaa6fa3307
sertmer <scottertmer@gmail.com>
2020-01-12 15:55:11 -0700

    add mapState test to ResearchContainer

--- src/Containers/ResearchContainer/ResearchContainer.test.js
+++ src/Containers/ResearchContainer/ResearchContainer.test.js
@@ -1,18 +1,56 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { ResearchContainer } from './ResearchContainer';
+import { ResearchContainer, mapStateToProps } from './ResearchContainer';
 
 describe('ResearchContainer', () => {
 
   let wrapper
+  let mockNasaImages
+  let mockCatImages
 
   beforeEach(() => {
-    wrapper = shallow(<ResearchContainer />)
+    mockNasaImages = [
+      {
+        id: 'ab1',
+        img: 'url.com',
+        dateCreated: '12-12-12'
+      }
+    ]
+
+    mockCatImages = [
+      {
+        id: 'ab2',
+        img: 'url2.com',
+        dateCreated: '12-13-12'
+      }
+    ]
+    wrapper = shallow(<ResearchContainer 
+      nasaImages={mockNasaImages} 
+      catImages={mockCatImages}
+    />)
   })
 
   describe('ResearchContainer unit tests', () => {
-    it.skip('should match the snapshot', () => {
+    it('should match the snapshot', () => {
       expect(wrapper).toMatchSnapshot()
+    })
+  })
+
+  describe('mapStateToProps', () => {
+    it('should return an objext with the images', () => {
+      const mockState = {
+        nasaImages: mockNasaImages,
+        catImages: mockCatImages,
+        user: 'Davey Havok'
+      }
+
+      const expected = {
+        nasaImages: mockNasaImages,
+        catImages: mockCatImages
+      }
+      
+      const mappedProps = mapStateToProps(mockState)
+      expect(mappedProps).toEqual(expected)
     })
   })
 })
